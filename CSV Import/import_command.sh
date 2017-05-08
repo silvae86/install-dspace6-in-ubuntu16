51d6f462c6e648239f0cc1c1ba06a43dc3b05c9c
João Rocha da Silva <joaorosilva@gmail.com>
2017-05-08 10:59:55 +0100

    Multiple values for a metadata descriptor added (loading in CSV)
    Added modifications for multiple author batch loading through CSV

--- CSV Import/import_command.sh
+++ CSV Import/import_command.sh
@@ -4,4 +4,4 @@ chmod -R ugo+w /dspace/dspace/imports
 
 cd /dspace/dspace/bin
 chmod +x ./dspace
-./dspace import -s /home/dspace/Registos\ Bibliografia\ Lyman\ Sargent\ -\ Metadata\ \(1\).csv -i csv -m /home/dspace/map -b -e lolada@gmail.com -c "123456789/4"
+./dspace import -s /home/dspace/template.csv -i csv -m /home/dspace/map -b -e lolada@gmail.com -c "123456789/4"
