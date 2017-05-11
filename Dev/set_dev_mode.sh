29d4fd2a2f9f937bcd5e04fff732bf4d76a858cd
João Rocha da Silva <joaorosilva@gmail.com>
2017-05-11 23:04:59 +0100

    Adding more steps for development
    Adding more steps for development

--- Dev/set_dev_mode.sh
+++ Dev/set_dev_mode.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env bash
 
+brew install maven ant
+
 brew install postgresql
 brew services start postgresql
+
 createdb dspace
 psql
 #CREATE EXTENSION pgcrypto;
 # \q to quit
+
+brew install solr
+brew services start solr
+
+#copy apps
+cd /usr/local/Cellar/tomcat/8.5.15/libexec/webapps/ &&
+cp -R /Users/joaorocha/NetBeansProjects/DSpace/dspace/target/dspace-installer/webapps/* . &&
+#start tomcat server
+/usr/local/Cellar/tomcat/8.5.15/bin/catalina run
+
+#add to dspace parent project's pom.xml file
+
+# <dependency>
+#     <groupId>xml-apis</groupId>
+#     <artifactId>xml-apis</artifactId>
+#     <version>1.4.01</version>
+# </dependency>
