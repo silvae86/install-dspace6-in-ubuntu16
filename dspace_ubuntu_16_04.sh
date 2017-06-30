b60dac387f17280ed3d32c606446e9d69ff3af56
João Rocha da Silva <joaorosilva@gmail.com>
2017-06-30 16:56:18 +0100

    Separated code into refresh and install
    Separated code into refresh and install

--- dspace_ubuntu_16_04.sh
+++ dspace_ubuntu_16_04.sh
@@ -69,33 +69,7 @@ exit
 # you can also edit the header jsp file to change the path to the logo
 #vim /dspace/webapps/jspui/layout/header-default.jsp
 
-sudo su
-su postgres
-date=$(date "+%Y.%m.%d-%H.%M.%S")
-pg_dump dspace > ~/dspace_bak_$date.sql
-exit
-#set the modification date to the current date on the server because of any time offset problems
-sudo su
-cd /home/DSpace
-touch -a /var/lib/tomcat8/webapps/jspui/image/*logo*
-cd /home/dspace/DSpace  &&
-mvn package &&
-cd /home/dspace/DSpace/dspace/target/dspace-installer &&
-#ant fresh_install &&
-ant update &&
-#copy app to installation directory
-cp -R /home/dspace/DSpace/dspace /dspace &&
-#install compiled apps in tomcat8
-#sudo cp $(find /home/dspace/DSpace | grep \.war$ | xargs echo) /var/lib/tomcat8/webapps;
-cd /var/lib/tomcat8/webapps &&
-rm -rf jspui/   oai/     rdf/     rest/    solr/    sword/   swordv2/ xmlui/ &&
-cp -R /dspace/dspace/webapps/jspui /dspace/dspace/webapps/solr /dspace/dspace/webapps/rest /var/lib/tomcat8/webapps &&
-touch -a /home/dspace/DSpace/dspace-jspui/src/main/webapp/image/*logo* &&
-#give ownership of installation to tomcat user
-sudo chown -R tomcat8 /dspace &&
-#restart tomcat;
-service tomcat8 restart &&
-echo "OK";
+source ./refresh_installation.sh
 
 #create admin user
 chmod +x /dspace/dspace/bin/dspace
---
+++ refresh_installation.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+sudo su
+su postgres
+date=$(date "+%Y.%m.%d-%H.%M.%S")
+pg_dump dspace > ~/dspace_bak_$date.sql
+exit
+#set the modification date to the current date on the server because of any time offset problems
+sudo su
+cd /home/DSpace
+touch -a /var/lib/tomcat8/webapps/jspui/image/*logo*
+cd /home/dspace/DSpace  &&
+mvn package &&
+cd /home/dspace/DSpace/dspace/target/dspace-installer &&
+#ant fresh_install &&
+ant update &&
+#copy app to installation directory
+cp -R /home/dspace/DSpace/dspace /dspace &&
+#install compiled apps in tomcat8
+#sudo cp $(find /home/dspace/DSpace | grep \.war$ | xargs echo) /var/lib/tomcat8/webapps;
+cd /var/lib/tomcat8/webapps &&
+rm -rf jspui/   oai/     rdf/     rest/    solr/    sword/   swordv2/ xmlui/ &&
+cp -R /dspace/dspace/webapps/jspui /dspace/dspace/webapps/solr /dspace/dspace/webapps/rest /var/lib/tomcat8/webapps &&
+touch -a /home/dspace/DSpace/dspace-jspui/src/main/webapp/image/*logo* &&
+#give ownership of installation to tomcat user
+sudo chown -R tomcat8 /dspace &&
+#restart tomcat;
+service tomcat8 restart &&
+echo "OK";
